2c682f651b339d3c5b0df544fca19c1ab67a79c1
muazdawud <muazdawud307@gmail.com>
2026-03-11 15:41:44 +0100

    Successful Analog input test

---
+++ Analog2Digital_test/LDR_AO_Controlled_LED/LDR_AO_ctrl_LED.c
@@ -0,0 +1,53 @@
+#include<avr/io.h>
+#include<util/delay.h>
+#include "USART.h"
+#include "pinDefines.h"
+
+#define T_DEL 500
+#define DURATION 1024
+
+static inline void initADC(void){
+	ADMUX |= (1 << REFS0);
+	ADCSRA &= ~(0xff);
+	ADCSRA |= (7 << ADPS0) | (1 << ADEN);
+}
+
+static void toggleLED(uint16_t period){
+	uint16_t delay_;
+	uint16_t i;
+
+	printByte(period);
+	printString("\r\n");
+
+	if(period){
+
+		for(delay_ = 0; delay_ < DURATION; delay_+=period){
+			for(i = 0; i < period; i++){
+				_delay_us(1);
+			}
+
+			LED_PORT ^= (1 << LED0);
+		}
+
+	}
+
+}
+
+int main(void){
+
+	LED_DDR |= (1 << LED0);
+
+	initADC();
+	initUSART();
+
+	printString("Transmitting LDR Resistance Value\r\n");
+
+	while(1){
+		ADCSRA |= (1 << ADSC);
+		loop_until_bit_is_clear(ADCSRA, ADSC);
+
+		toggleLED(ADC);
+	}
+
+	return 0;
+}
---
+++ Analog2Digital_test/UART_LDR_Value/UART_LDR_Value.c
@@ -0,0 +1,33 @@
+#include<avr/io.h>
+#include<util/delay.h>
+#include "USART.h"
+#include "pinDefines.h"
+
+#define TRANSMIT_DELAY 500
+#define PORTB_PIN 0x3f
+
+static inline void initADC(void){
+	ADMUX |= (1 << REFS0);
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+	ADCSRA |= (1 << ADEN);
+}
+
+int main(void){
+
+	initUSART();
+	initADC();
+
+	printString("Transmitting LDR Resistance Values;\r\n");
+
+	while(1){
+		ADCSRA |= (1 << ADSC);
+		loop_until_bit_is_clear(ADCSRA, ADSC);
+
+		printByte(ADC);
+		printString("\r\n");
+
+		_delay_ms(TRANSMIT_DELAY);
+	}
+
+	return 0;
+}
