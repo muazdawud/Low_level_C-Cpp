29d9b7c4fa50bec2f55925258cfb5eff700e215b
muazdawud <muazdawud307@gmail.com>
2026-04-20 20:48:11 +0100

    Resolved some of the issues regarding not showing temp and humd

--- Random_Tests/DHT11_Sensor/src/DHT_11_TH.c
+++ Random_Tests/DHT11_Sensor/src/DHT_11_TH.c
@@ -9,11 +9,17 @@
 #include<avr/power.h>
 
 
-#include "USART_D.h"
 #include "DHT_11_TH.h"
 #include "reg_defs_t.h"
  
- 
+
+typedef struct {
+    volatile uint8_t* port;
+    volatile uint8_t* ddr;
+    volatile uint8_t* pin;
+    uint8_t bit;
+} DHT_Device;
+
 DHT_Device mySensor;
 
 
@@ -30,7 +36,8 @@ volatile static uint8_t temp_t_d = 0;
 volatile static uint8_t temp_h_d = 0;
 
 
-volatile static uint8_t timestamp;
+volatile static uint8_t timerGuard = 0;
+volatile static uint16_t timestamp = 0;
 volatile static uint8_t pcint_cycle = 0;
 volatile static uint8_t bit_position_tnhd = 0; /*
 						It is used to track the exact position of the data\
@@ -43,64 +50,88 @@ volatile static uint8_t bit_position_tnhd = 0; /*
  
 static void DHT_StartSignal(void);
 static void DHT_Parse_Data(void); 
-static uint8_t DHT_Verify_Checksum(void);
+static inline uint8_t DHT_Verify_Checksum(void);
 static inline void initTIMER_1(void);
 
 
 void DHT_Init(uint8_t portValue, uint8_t PIN);
 void DHT_HandleSignal(void);
-void DHT_ReadSignal(void);
 uint8_t DHT_Get_Temp(void);
 uint8_t DHT_Get_Humidity(void); 
 
 
 
+// ISR(_TIMER1_OVF_){
+
+// 	timerGuard++;
+
+// 	if(timerGuard >= 120){
+// 		timerGuard = 0;
+// 		_TCR1B_ = 0;
+// 	}
+// }
+
+
+
 static void DHT_StartSignal(void) {
 
-	// USART_print("	-> Inside DHT_StartSignal().\r\n");
+	_TCR1B_ = 0;
+	TCNT1 = 0;
+
+	temp_t = temperature;
+	temp_t_d = temp_deci;
+	temp_h = humidity;
+	temp_h_d = hum_deci;
 
 	timestamp = 0;
 	pcint_cycle = 0;
+	timerGuard = 0;
+	bit_position_tnhd = 0;
+	temp_nd_hum_data = 0;
+	checksum = 0;
 
     *(mySensor.ddr) |= (1 << mySensor.bit);
     *(mySensor.port) &= ~(1 << mySensor.bit);
     _delay_ms(MCU_BD_LOW);
 
     *(mySensor.port) |= (1 << mySensor.bit);
+    _delay_us(MCU_BD_HIGH);
+    *(mySensor.ddr) &= ~(1 << mySensor.bit);
 
-    sei();
     _TCR1B_ |= (1 << _CS11_);
-    TCNT1 = 0;
+    
+    sei();
 }
 
 
 static void DHT_Parse_Data(void){
 
-	USART_print("	-> Inside DHT_Parse_Data().\r\n");
+	uint8_t temp = SREG;
+	cli();
 
 	if(temp_nd_hum_data){
 
-		humidity = (temp_nd_hum_data);
+		humidity = (temp_nd_hum_data >> 24);
 
-		hum_deci = (temp_nd_hum_data >> 8);
+		hum_deci = (temp_nd_hum_data >> 16);
 
-		temperature = (temp_nd_hum_data >> 16);
+		temperature = (temp_nd_hum_data >> 8);
 
-		temp_deci = (temp_nd_hum_data >> 24);
+		temp_deci = (temp_nd_hum_data);
 
-		if(!DHT_Verify_Checksum()){
-			humidity = temp_h;
-			hum_deci = temp_h_d;
-			temperature = temp_t;
-			temp_deci = temp_t_d;
-		}
+		// if(!DHT_Verify_Checksum()){
+		// 	humidity = temp_h;
+		// 	hum_deci = temp_h_d;
+		// 	temperature = temp_t;
+		// 	temp_deci = temp_t_d;
+		// }
 	}
+
+	SREG = temp;
 }
 
 
-static uint8_t DHT_Verify_Checksum(void){
-
-	USART_print("	-> Inside DHT_Verify_Checksum().\r\n");
+static inline uint8_t DHT_Verify_Checksum(void){
 
 	uint8_t sum = humidity + hum_deci + temperature + temp_deci;
 
@@ -116,15 +147,13 @@ static inline void initTIMER_1(void){
 
 	_TCR1B_ &= ~(1 << _WGM12_) & ~(1 << _WGM13_);
 	_TCR1A_ &= ~(1 << _WGM10_) & ~(1 << _WGM11_);
+	// _TIMSK1_ |= (1 << _TOIE1_);
 }
 
 
 
 
-
 void DHT_Init(uint8_t portSelection, uint8_t pinNum) {
-
-	USART_print("	-> Inside DHT_Init().\r\n");
 
 	clock_prescale_set(clock_div_1);
 
@@ -166,53 +195,45 @@ void DHT_Init(uint8_t portSelection, uint8_t pinNum) {
 
 void DHT_HandleSignal(void){ 
 
-	if(pcint_cycle < 2){
-		*(mySensor.ddr) &= ~(1 << mySensor.bit);
-		pcint_cycle++;
+	if(pcint_cycle > 2){
+
+		if(bit_is_set(*(mySensor.pin), mySensor.bit)){
+
+			timestamp = TCNT1;
+		}
+		else{
+			uint16_t temp = TCNT1 - timestamp;
+
+			uint8_t bit_ = (temp >  50) ? 1 : 0;
+		 
+			if(bit_position_tnhd < 32){
+
+					temp_nd_hum_data = (temp_nd_hum_data << 1) | bit_;
+			}
+
+			else if(bit_position_tnhd < 40){
+
+					checksum = (checksum << 1) | bit_;
+			}
+
+			bit_position_tnhd++;
+		}
 	}
 
-	else{
-
-		timestamp = TCNT1;
-	}
-}
-
-
-void DHT_ReadSignal(void){
-
-	uint16_t temp = TCNT1 - (timestamp);
-
-	uint8_t bit_ = (temp >  90) ? 1 : 0;
- 
-	if(bit_position_tnhd < 32){
-
-			temp_nd_hum_data = (temp_nd_hum_data << 1) | bit_;
-	}
-
-	else if(bit_position_tnhd < 40){
-
-			checksum = (checksum << 1) | bit_;
-	}
-
-	bit_position_tnhd++;
+	pcint_cycle++;
 }
 
 
 uint8_t DHT_Get_Temp(void){
 
-	USART_print("	-> Getting Temperature.\r\n");
-
-	temp_t = temperature;
-	temp_t_d = temp_deci;
-	temp_h = humidity;
-	temp_h_d = hum_deci;
+	// if(timerGuard){
+	// 	return temperature;
+	// }
 
 	DHT_StartSignal();
 	_delay_ms(DHT_READ_DELAY - MCU_BD_LOW);
 
 	DHT_Parse_Data();
-
-	USART_print("	-> Temperature in DHT_Get_Temp() = %d.\r\n", temperature);
 
 	//cli();
 
@@ -222,19 +243,14 @@ uint8_t DHT_Get_Temp(void){
 
 uint8_t DHT_Get_Humidity(void){
 
-	USART_print("	-> Getting Humidity.\r\n");
-
-	temp_t = temperature;
-	temp_t_d = temp_deci;
-	temp_h = humidity;
-	temp_h_d = hum_deci;
+	// if(timerGuard){
+	// 	return humidity;
+	// }
 
 	DHT_StartSignal();
 	_delay_ms(DHT_READ_DELAY - MCU_BD_LOW);
 
 	DHT_Parse_Data();
-
-	USART_print("	-> Humidity in DHT_Get_Humidity() = %d.\r\n", humidity);
 
 	//cli();
 
