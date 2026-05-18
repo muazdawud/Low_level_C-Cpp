f22a1fbd09da03a84d751523601536589427b2b6
muazdawud <muazdawud307@gmail.com>
2026-05-18 10:24:13 +0100

    Initial PWM tests

---
+++ PWM_Output/pwm1/pwm.c
@@ -0,0 +1,63 @@
+
+#include<avr/io.h>
+#include<util/delay.h>
+
+
+
+#define DELAY 10000      // This value is in micro-seconds
+
+static void initTimers(void){
+
+
+	TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
+	TCCR0A |= (1 << WGM01) | (1 << WGM00);
+	TCCR0B |= (1 << CS01);
+
+
+	TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
+	TCCR1A |= (1 << WGM10);
+	TCCR1B |= (1 << WGM12);
+	TCCR1B |= (1 << CS11);
+
+
+	TCCR2A |= (1 << COM2A1) | (1 << COM2B1);
+	TCCR2A |= (1 << WGM21) | (1 << WGM20);
+	TCCR2B |= (1 << CS21);
+}
+
+
+int main(void){
+
+	uint8_t i;
+	int8_t direction = 1;
+
+	initTimers();
+
+	DDRB |= (1 << PB1) | (1 << PB2) | (1 << PB3);
+	DDRD |= (1 << PD3) | (1 << PD5) | (1 << PD6);
+
+	while(1){
+
+		for(i = 0; i < 256; i += direction){
+
+			if(i == 0){
+				direction = 1;
+			}
+
+			if(i == 255){
+				direction = -1;			
+			}
+
+			OCR2B = OCR0B;
+		    OCR0B = OCR1A;
+		    OCR1A = OCR1B;
+		    OCR2A = OCR0A;
+		    OCR1B = OCR2A;
+		    OCR0A = i;
+
+		    _delay_us(DELAY);
+		}
+	}
+
+	return 0;
+}
