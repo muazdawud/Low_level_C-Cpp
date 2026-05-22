45007a86104251b9cfd7a8adc20972fb771ff871
muazdawud <muazdawud307@gmail.com>
2026-05-22 16:13:16 +0100

    All test changes and callibration done

--- servoControl/servoTest/servo.c
+++ servoControl/servoTest/servo.c
@@ -12,73 +12,49 @@
 #include "USART.h"
 
 
-#define  PULSE_MIN  2000
-#define  PULSE_MID  3000
-#define  PULSE_MAX  4000
+#define  F_OCPWM  50
+#define  PRESCALER  64
+#define  TOP  ((F_CPU / (F_OCPWM * PRESCALER)) - 1)
+
+
+#define  PULSE_MIN  ((TOP / 20) * 1)
+#define  PULSE_MID  ((TOP / 20) * 1.5)
+#define  PULSE_MAX  ((TOP / 20) * 2)
 
 uint16_t delay_value = 0;
-
-// ISR(TIMER1_COMPA_vect){
-
-// 	PORTB |= (1 << PB1);
-// 	_delay_us(delay_value);
-// 	PORTB &= ~(1 << PB1);
-// }
-
 
 static inline uint16_t getNumber16(void);
 
 static inline void initTimer(void){
 
 	TCCR1A |= (1 << COM1A1);
+
 	TCCR1A |= (1 << WGM11);
 	TCCR1B |= (1 << WGM12) | (1 << WGM13);
 
-	TCCR1B |= (1 << CS11);
+	TCCR1B |= (1 << CS11) | (1 << CS10);
 
-	// TIMSK1 |= (1 << OCIE1A);
-
-	ICR1 = 40000;
-
-	// OCR1A = PULSE_MID; 
-
-	// sei();
+	ICR1 = TOP;
 }
 
 
 static void showOff(void){
 
-	// ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+	printString("Servo in Center\r\n");
+	OCR1A = PULSE_MID;
+	_delay_ms(1500);
 
-		printString("Servo in Center\r\n");
-		// PORTB |= (1 << PB1);
-		// _delay_us(PULSE_MID);
-		// PORTB &= ~(1 << PB1);
-		OCR1A = PULSE_MID;
-		_delay_ms(1500);
+	printString("Servo Clockwise Max\r\n");
+	OCR1A = PULSE_MAX;
+	_delay_ms(1500);
 
-		printString("Servo Clockwise Max\r\n");
-		// PORTB |= (1 << PB1);
-		// _delay_us(PULSE_MAX);
-		// PORTB &= ~(1 << PB1);
-		OCR1A = PULSE_MAX;
-		_delay_ms(1500);
+	printString("Servo Counter-Clockwise Max\r\n");
+	OCR1A = PULSE_MIN;
+	_delay_ms(1500);
 
-		printString("Servo Counter-Clockwise Max\r\n");
-		// PORTB |= (1 << PB1);
-		// _delay_us(PULSE_MIN);
-		// PORTB &= ~(1 << PB1);
-		OCR1A = PULSE_MIN;
-		_delay_ms(1500);
-
-		printString("Servo in Center\r\n");
-		// PORTB |= (1 << PB1);
-		// _delay_us(PULSE_MID);
-		// PORTB &= ~(1 << PB1);
-		OCR1A = PULSE_MID;
-		_delay_ms(1500);
-
-	// }
+	printString("Servo in Center\r\n");
+	OCR1A = PULSE_MID;
+	_delay_ms(1500);
 }
 
 
@@ -98,7 +74,7 @@ int main(void){
 
 		DDRB &= ~(1 << PB1);
 
-		printString("Enter a number between 2000-4000\r\n");
+		printString("Enter a number between 128-620\r\n");
 
 		input = getNumber16();
 		OCR1A = input;
@@ -108,7 +84,7 @@ int main(void){
 		_delay_ms(1000);
 	    printString("Releasing...\r\n");
 
-	    while (TCNT1 < 15000){}
+	    while (TCNT1 < (PULSE_MAX * 2)){}
 	}
 
 	return 0;
