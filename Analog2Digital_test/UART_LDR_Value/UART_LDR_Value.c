9fdec772a3f75f8c6bd0149e24074856822c0480
muazdawud <muazdawud307@gmail.com>
2026-05-23 16:09:06 +0100

    Tiny modification to fit Joystick value read

--- Analog2Digital_test/UART_LDR_Value/UART_LDR_Value.c
+++ Analog2Digital_test/UART_LDR_Value/UART_LDR_Value.c
@@ -7,7 +7,7 @@
 #define PORTB_PIN 0x3f
 
 static inline void initADC(void){
-	ADMUX |= (1 << REFS0);
+	// ADMUX |= (1 << REFS0);
 	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 	ADCSRA |= (1 << ADEN);
 }
@@ -16,6 +16,9 @@ int main(void){
 
 	initUSART();
 	initADC();
+
+	uint16_t x_axis = 0;
+	uint16_t y_axis = 0;
 
 	printString("Transmitting LDR Resistance Values;\r\n");
 
